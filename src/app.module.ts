65204eaa458fb8f19dd48d793d1b070645a12c72
Misza Pawlik <zgredziczek@gmail.com>
2024-07-03 15:52:03 +0200

    Add mongoDB configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProblemsModule } from './problems/problems.module';
 
 @Module({
-  imports: [ProblemsModule],
+  imports: [
+    ProblemsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/city-fixer-db'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
